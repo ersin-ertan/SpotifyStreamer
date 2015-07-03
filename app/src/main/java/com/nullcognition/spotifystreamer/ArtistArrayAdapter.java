93a498de675dc055148364ab9f74bb134ebf8add
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-07-03 18:19:40 -0400

    using viewholder pattern to hold images

--- app/src/main/java/com/nullcognition/spotifystreamer/ArtistArrayAdapter.java
+++ app/src/main/java/com/nullcognition/spotifystreamer/ArtistArrayAdapter.java
@@ -21,92 +21,46 @@ public class ArtistArrayAdapter extends ArrayAdapter<ArtistListItem>{
 	public ArtistArrayAdapter(final Context context, final ArrayList<ArtistListItem> resource){
 		super(context, 0, resource);
 	}
-	//	@Override
-//	public View getView(final int position, final View convertView, final ViewGroup parent){
-//		View rootView = LayoutInflater.from(getContext()).inflate(R.layout.search_list_item, parent, false);
-//
-//		ArtistListItem ali = getItem(position);
-//
-//		((TextView) rootView.findViewById(R.id.textView)).setText(ali.getArtistName());
-//
-//		ImageView[] images = new ImageView[4];
-//		images[0] = (ImageView) rootView.findViewById(R.id.imageView1);
-//		images[1] = (ImageView) rootView.findViewById(R.id.imageView2);
-//		images[2] = (ImageView) rootView.findViewById(R.id.imageView3);
-//		images[3] = (ImageView) rootView.findViewById(R.id.imageView4);
-//
-//
-//		int size = ali.getTop4Images().size();
-//		for(int i = 0; i < size; i++){
-//			Picasso.with(getContext()).load(ali.getTop4Images().get(i).url).fit().into(images[i]);
-//
-//		}
-//		return rootView;
-//
-//	}
+
 	@Override
 	public View getView(final int position, final View convertView, final ViewGroup parent){
-		View rootView = LayoutInflater.from(getContext()).inflate(R.layout.search_list_item, parent, false);
 
+
+		View rootView;
+		Context context = getContext();
+
+		if(convertView == null){
+			rootView = LayoutInflater.from(context).inflate(R.layout.search_list_item, parent, false);
+			ViewHolder viewHolder = new ViewHolder();
+			viewHolder.textView = (TextView) rootView.findViewById(R.id.textView);
+
+			ImageView[] images = new ImageView[4];
+			images[0] = (ImageView) rootView.findViewById(R.id.imageView1);
+			images[1] = (ImageView) rootView.findViewById(R.id.imageView2);
+			images[2] = (ImageView) rootView.findViewById(R.id.imageView3);
+			images[3] = (ImageView) rootView.findViewById(R.id.imageView4);
+			viewHolder.imageViews = images;
+			rootView.setTag(viewHolder);
+		}
+		else{ rootView = convertView; }
+
+		ViewHolder viewHolder = (ViewHolder) rootView.getTag();
 		ArtistListItem ali = getItem(position);
 
-		((TextView) rootView.findViewById(R.id.textView)).setText(ali.getArtistName());
-
-		ImageView[] images = new ImageView[4];
-		images[0] = (ImageView) rootView.findViewById(R.id.imageView1);
-		images[1] = (ImageView) rootView.findViewById(R.id.imageView2);
-		images[2] = (ImageView) rootView.findViewById(R.id.imageView3);
-		images[3] = (ImageView) rootView.findViewById(R.id.imageView4);
-
+		viewHolder.textView.setText(ali.getArtistName());
 
 		int size = ali.getTop4Images().size();
 		for(int i = 0; i < size; i++){
-			Picasso.with(getContext()).load(ali.getTop4Images().get(i).url).fit().into(images[i]);
+			Picasso.with(getContext()).load(ali.getTop4Images().get(i).url).fit().into(viewHolder.imageViews[i]);
 		}
 		if(size > 0){ // in case the size is 0, there is no default '0' url to use
+			String url = ali.getTop4Images().get(0).url;
 			for(int i = size; i < 4; i++){ // fill the empties with the first art
-				Picasso.with(getContext()).load(ali.getTop4Images().get(0).url).fit().into(images[i]);
+				Picasso.with(getContext()).load(url).fit().into(viewHolder.imageViews[i]);
 			}
 		}
 		return rootView;
 
 	}
-
-	//	@Override
-//	public View getView(int position, View convertView, ViewGroup parent){
-//
-//		View rootView;
-//		Context context = getContext();
-//
-//		if(convertView == null){
-//			rootView = LayoutInflater.from(context).inflate(R.layout.search_list_item, parent, false);
-//			ViewHolder viewHolder = new ViewHolder();
-//
-//			viewHolder.textView = (TextView) rootView.findViewById(R.id.textView);
-//
-//			ImageView[] images = new ImageView[4];
-//			images[0] = (ImageView) rootView.findViewById(R.id.imageView1);
-//			images[1] = (ImageView) rootView.findViewById(R.id.imageView2);
-//			images[2] = (ImageView) rootView.findViewById(R.id.imageView3);
-//			images[3] = (ImageView) rootView.findViewById(R.id.imageView4);
-//			viewHolder.imageViews = images;
-//			rootView.setTag(viewHolder);
-//		}
-//		else{ rootView = convertView; }
-//
-//		ViewHolder viewHolder = (ViewHolder) rootView.getTag();
-//		ArtistListItem artistListItem = getItem(position);
-//
-//		viewHolder.textView.setText(artistListItem.getArtistName());
-//
-//		ArrayList<Image> top4Images = artistListItem.getTop4Images();
-//		Picasso.with(context).load(top4Images.get(0).url).into(viewHolder.imageViews[0]);
-//		// todo make the first image bigger than the rest
-//		for(int i = 1; i < 4; i++){
-//			Picasso.with(context).load(top4Images.get(i).url).into(viewHolder.imageViews[i]);
-//		}
-//		return convertView;
-//	}
-
 
 }
