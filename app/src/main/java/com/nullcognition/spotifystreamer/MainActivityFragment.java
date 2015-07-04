cc18ed6472ef603ca542012080a4c0237554d03d
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-07-04 01:28:06 -0400

    streamline the view to one instead of 4, bug of views not being populated because of 0 sized top 4 image lists, <-fixed the bug  by  explicitly setting the view if it was 0 sized

--- app/src/main/java/com/nullcognition/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/nullcognition/spotifystreamer/MainActivityFragment.java
@@ -1,28 +1,20 @@
 package com.nullcognition.spotifystreamer;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewStub;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 import de.greenrobot.event.EventBus;
-import kaaes.spotify.webapi.android.models.Image;
 
-/**
- A placeholder fragment containing a simple view.
- */
 public class MainActivityFragment extends Fragment{
-
-	View emptyView = null;
 
 	public MainActivityFragment(){ }
 
@@ -30,79 +22,77 @@ public class MainActivityFragment extends Fragment{
 	public void onCreate(final Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		EventBus.getDefault().register(this);
-//		setRetainInstance(true); // is this needed to save the results on rotation?
-		IntentServiceArtistSearch.searchByArtistName(getActivity(), "Paul");
-
-
-
-	}
-	private void testAdapter(){
-		HashMap<String, ArrayList<Image>> hm = new HashMap<>();
-		ArrayList<Image> ar = new ArrayList<Image>();
-		Image i = new Image();
-		i.height = 64;
-		i.width = 64;
-		i.url = "http://people.mozilla.org/~faaborg/files/shiretoko/firefoxIcon/firefox-64-noshadow.png";
-		ar.add(i);
-		ar.add(i);
-		ar.add(i);
-		ar.add(i); // need 4
-		hm.put("test", ar);
-		ArtistListItemData a = new ArtistListItemData(hm);
-		populateAdapter( a);
 	}
 
 	ListView listView;
+	View inflated;
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+	public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
 		View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 		listView = (ListView) rootView.findViewById(R.id.listView);
 		View header = LayoutInflater.from(getActivity()).inflate(R.layout.header_ad, container);
 		listView.addHeaderView(header);
+		// using AdView as header causes app to slow for list interactions
 
-		// test to see if changing the adapter auto refreshes the table, yes it does
-//		listView.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, new String[]{".,", ",.p", ",.p"}));
-		// testing
-//		testAdapter();
-// if(emptyView == null){
-//			emptyView = ((ViewStub) rootView.findViewById(R.id.empty)).inflate();
-//		}
+		ViewStub stub = (ViewStub) rootView.findViewById(R.id.empty);
+		inflated = stub.inflate();
 
 		((EditText) rootView.findViewById(R.id.editText)).addTextChangedListener(new TextWatcher(){
+			Handler handler = new Handler();
+			Runnable latestPost;
 			@Override
-			public void beforeTextChanged(final CharSequence s, final int start, final int count, final int after){
-
-			}
+			public void beforeTextChanged(final CharSequence s, final int start, final int count, final int after){ }
 			@Override
-			public void onTextChanged(final CharSequence s, final int start, final int before, final int count){
-
-			}
+			public void onTextChanged(final CharSequence s, final int start, final int before, final int count){}
 			@Override
 			public void afterTextChanged(final Editable s){
-//				Toast.makeText(getActivity(), "text changed", Toast.LENGTH_SHORT).show();
-
-
+				if(s != null && !s.toString().equals("") && s.length() > 0){
+					if(latestPost != null){ handler.removeCallbacks(latestPost); }
+					latestPost = new Runnable(){
+						@Override
+						public void run(){
+							if(!s.toString().equals("") && s.length() > 0){ // need to check for s content data change
+								IntentServiceArtistSearch.searchByArtistName(getActivity(), s.toString());
+								if(inflated.getVisibility() == View.VISIBLE){
+									inflated.setVisibility(View.INVISIBLE); // add an ad
+								}
+							}
+						}
+					};
+					handler.postDelayed(latestPost, 750); // in milliseconds, leaves enough time for to write
+					// but still searches fast enough to stay responsive, conserve usage as well
+				}
+				else if(s != null && s.length() == 0 && latestPost == null){
+					inflated.setVisibility(View.VISIBLE);
+				}
 			}
 		});
 		return rootView;
 	}
 
-	public void populateAdapter(final ArtistListItemData a){
-		listView.setAdapter(new ArtistArrayAdapter(getActivity(), a.getArrayOfArtistListItems()));
-		Toast.makeText(getActivity(), "populateAdapter", Toast.LENGTH_SHORT).show();
-
-	}
-
-
-	// Get input from an edit text and put a listener on it for 2 seconds after input is finished
-	// which will auto search/call intent service
 	public void onEventMainThread(ArtistListItemData artistListItemData){
 		if(artistListItemData != null){
 			ArtistArrayAdapter arrayAdapter = new ArtistArrayAdapter(getActivity(), artistListItemData.getArrayOfArtistListItems());
 			listView.setAdapter(arrayAdapter);
-//			if(emptyView != null){ emptyView.setVisibility(View.INVISIBLE); }
+			listView.invalidate();
 		}
 	}
 }
+
+//	private void testAdapter(){
+//		HashMap<String, ArrayList<Image>> hm = new HashMap<>();
+//		ArrayList<Image> ar = new ArrayList<Image>();
+//		Image i = new Image();
+//		i.height = 64;
+//		i.width = 64;
+//		i.url = "http://people.mozilla.org/~faaborg/files/shiretoko/firefoxIcon/firefox-64-noshadow.png";
+//		ar.add(i);
+//		ar.add(i);
+//		ar.add(i);
+//		ar.add(i); // need 4
+//		hm.put("test", ar);
+//		ArtistListItemData a = new ArtistListItemData(hm);
+//		populateAdapter(a);
+//	}
